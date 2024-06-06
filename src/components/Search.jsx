1bd343fa5cd20448004e89d8b6bc935c802ae0a7
Neeraj kumar <neerajkumar08697@gmail.com>
2024-06-06 06:36:56 +0530

    update search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,13 +8,13 @@ const Search = () => {
 
   return (
     <div>
-      <div className="w-full flex justify-center bg-gray-200">
-        <div className="input  mt-5 mb-3 rounded-lg shadow-lg ">
+      <div className="w-full flex justify-center bg-gray-200  max-sm:justify-center   ">
+        <div className="input  mt-5 mb-3 rounded-lg shadow-lg max-sm:w-[380px]">
           <input
             type="text"
             placeholder="Search Product"
             onChange={(e) => setQuary(e.target.value)}
-            className="outline-none w-[500px] py-5 px-3 rounded-lg text-xl"
+            className="outline-none w-[500px] py-5 px-3 rounded-lg text-xl  max-sm:w-[380px]"
           />
         </div>
       </div>
